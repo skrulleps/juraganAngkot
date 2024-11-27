a97519f35d84125047836e82669f994744d145a6
unknown <fadhlantuhairi@gmail.com>
2024-11-27 12:01:08 +0700

    juragan angkot object

---
+++ angkot.js
@@ -0,0 +1,28 @@
+function Angkot(sopir, trayek, penumpang, kas){
+    this.sopir = sopir;
+    this.trayek = trayek;
+    this.penumpang = penumpang;
+    this.kas = kas;
+
+    this.penumpangNaik = function(namaPenumpang){
+        this.penumpang.push(namaPenumpang);
+        return this.penumpang;
+    }
+
+    this.penumpangTurun = function(namaPenumpang, bayar) {
+        if(this.penumpang.length === 0){
+            alert('angkot masih kosong');
+            return false;
+        }
+
+        for( var i = 0 ; i < this.penumpang.length; i++){
+            if(this.penumpang[i] == namaPenumpang){
+                this.penumpang[i] = undefined;
+                this.kas += bayar;
+            }
+        }
+    }
+}
+
+var angkot1 = new Angkot('Fadhlan', ['Tangerang', 'Cikokol'], [], 0);
+var angkot2 = new Angkot('Tuhairi', ['Rajeg', 'Kotbum'], [], 0);
